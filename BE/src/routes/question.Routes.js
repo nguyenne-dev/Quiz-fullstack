87e0a92035bafb602edd6546d4e353d380c72596
Nguyên <nguyenne.dev@gmail.com>
2025-08-09 10:07:53 +0700

    Them API quan ly bo cau hoi

---
+++ BE/src/routes/question.Routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const questionController = require("../controllers/question.Controller");
+const authMiddleware = require("../middlewares/authMiddleware");
+const checkAdmin = require("../middlewares/checkAdmin")
+
+router.get("/all", authMiddleware, checkAdmin, questionController.getQuestion);
+router.get("/:topicId", questionController.getQuestionTopic);
+router.post("/", authMiddleware, checkAdmin, questionController.addQuestion);
+router.patch("/:id", authMiddleware, checkAdmin, questionController.updateQuestion);
+router.delete("/:id", authMiddleware, checkAdmin, questionController.deleteQuestion);
+
+module.exports = router;
